45348b9dfc9b236cacc24e2db558782e14bfd8b4
Bishal99-pj <bishal1999mishra@gmail.com>
2024-05-01 00:59:47 +0530

    Added: default cluster (supercluster, geojson)

--- pages/branches/index.vue
+++ pages/branches/index.vue
@@ -9,6 +9,9 @@
 
 <script setup lang="ts">
 //-------- TYPES
+import type * as GeoJSON from "geojson";
+import Supercluster from "supercluster";
+
 import type { Geoposition } from "~/types/Branch.type";
 declare global {
   interface Window {
@@ -29,6 +32,17 @@ const center = computed<woosmap.map.LatLngLiteral>(() => ({
 // Initialize and add the map
 let map: woosmap.map.Map;
 
+let infoWindow: woosmap.map.InfoWindow;
+let index: Supercluster;
+let markers: woosmap.map.Marker[] = [];
+const ICON_URL = "https://images.woosmap.com/icons/pin-green.png";
+const SUPERCLUSTER_OPTIONS: Supercluster.Options<any, any> = {
+  radius: 30,
+  extent: 256,
+  maxZoom: 15,
+  minPoints: 2,
+};
+
 //-------- METHODS
 function getCurrentLocation() {
   navigator.geolocation.getCurrentPosition(
@@ -45,10 +59,163 @@ function getCurrentLocation() {
 }
 // woosmap init
 function initMap(): void {
+  infoWindow = new woosmap.map.InfoWindow({});
   map = new woosmap.map.Map(woosmapRef.value as HTMLElement, {
     zoom: 13,
     center: center.value,
   });
+
+  getAllBranches().then((stores) => {
+    index = new Supercluster(SUPERCLUSTER_OPTIONS).load(stores);
+    update();
+  });
+
+  manageEvents();
+}
+
+// Store service
+function getAllBranches(): Promise<woosmap.map.stores.StoreResponse[]> {
+  const allStores: woosmap.map.stores.StoreResponse[] = [];
+
+  const query: woosmap.map.stores.StoresSearchRequest = {
+    storesByPage: 300,
+  };
+  const storesService = new woosmap.map.StoresService();
+
+  // GET PAGINATED STORES
+  const fetchStores = async (
+    storePage?: number
+  ): Promise<woosmap.map.stores.StoreResponse[]> => {
+    if (storePage) {
+      query.page = storePage;
+    }
+    return storesService
+      .search(query)
+      .then((response: woosmap.map.stores.StoresSearchResponse) => {
+        
+        // TODO: handle initial cluster of branches
+        allStores.push(...response.features);
+
+        if (query.page === response.pagination.pageCount) {
+          return allStores;
+        }
+        return fetchStores(response.pagination.page + 1);
+      })
+      .catch((err: Error) => {
+        console.error(err);
+        throw new Error(`Error getting all stores: ${err.message}`);
+      });
+  };
+  return fetchStores();
+}
+
+// Map Events
+function manageEvents(): void {
+  map.addListener("dragend", useDebounceFn(update, 20));
+  map.addListener("zoom_changed", useDebounceFn(update, 20));
+  window.addEventListener("resize", useDebounceFn(update, 100));
+}
+
+function update(): void {
+  clearMarkers();
+  const bounds = map.getBounds();
+  // geographical bounds
+  const bbox: GeoJSON.BBox = [
+    bounds.getSouthWest().lng(),
+    bounds.getSouthWest().lat(),
+    bounds.getNorthEast().lng(),
+    bounds.getNorthEast().lat(),
+  ];
+  const clusterData = index.getClusters(bbox, map.getZoom());
+
+  // update markers
+  for (const feature of clusterData) {
+    const latlng = {
+      lat: feature.geometry.coordinates[1],
+      lng: feature.geometry.coordinates[0],
+    };
+    //
+    markers.push(createClusterIcon(feature, latlng));
+  }
+}
+
+function clearMarkers(): void {
+  for (const marker of markers) {
+    marker.setMap(null);
+  }
+  markers = [];
+}
+
+function createClusterIcon(
+  feature: any,
+  latlng: woosmap.map.LatLngLiteral
+): woosmap.map.Marker {
+  if (!feature.properties.cluster) {
+    return createMarker(feature, latlng);
+  }
+  return createCluster(feature, latlng);
+}
+
+function createCluster(
+  feature: any,
+  latlng: woosmap.map.LatLngLiteral
+): woosmap.map.Marker {
+  const count = feature.properties.point_count;
+
+  const size = count < 80 ? 45 : count < 400 ? 55 : 65;
+
+  const svg = window.btoa(`
+<svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 240 240">
+  <circle cx="120" cy="120" opacity=".8" r="70" />
+  <circle cx="120" cy="120" opacity=".3" r="80" />
+  <circle cx="120" cy="120" opacity=".2" r="110" />
+</svg>`);
+
+  const marker = new window.woosmap.map.Marker({
+    label: {
+      text: feature.properties["point_count_abbreviated"],
+      color: "white",
+    },
+    position: latlng,
+    icon: {
+      url: `data:image/svg+xml;base64,${svg}`,
+      scaledSize: new window.woosmap.map.Size(size, size),
+    },
+    map: map,
+  });
+
+
+  marker.addListener("click", (e) => {
+    infoWindow.close();
+    const expansionZoom = index.getClusterExpansionZoom(
+      feature.properties["cluster_id"]
+    );
+    map.setCenter(marker.getPosition());
+    map.setZoom(expansionZoom);
+  });
+  return marker;
+}
+
+function createMarker(
+  feature: any,
+  latlng: woosmap.map.LatLngLiteral
+): woosmap.map.Marker {
+  const marker = new window.woosmap.map.Marker({
+    icon: {
+      url: ICON_URL,
+      scaledSize: {
+        height: 20,
+        width: 20,
+      },
+    },
+    position: latlng,
+    map: map,
+  });
+  marker.addListener("click", () => {
+    infoWindow.setContent(`<h3>${feature.properties.name}</h3>`);
+    infoWindow.open(map, marker);
+  });
+  return marker;
 }
 
 //-------- WATCHERS
